62dd89046d244bdeb57d3249220668cd23571bc7
Guillaume Pirou <guillaume.pirou@epitech.eu>
2018-03-05 14:21:21 +0000

    add game 2 of principal agent

--- settings.py
+++ settings.py
@@ -138,6 +138,12 @@ SESSION_CONFIGS = [
         'display_name': 'Principal agent relations: Game 1',
         'num_demo_participants': 6,
         'app_sequence': ['principal_agent_relations_1', 'payment_info']
+    },
+    {
+        'name': 'principal_agent_relations_2',
+        'display_name': 'Principal agent relations: Game 2',
+        'num_demo_participants': 6,
+        'app_sequence': ['principal_agent_relations_2', 'payment_info']
     }
 ]
 
